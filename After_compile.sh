6b292a69ab09115de55da1c03e45a8eccdced187
Hadesatan <540224282@qq.com>
2026-08-09 12:43:42 +0800

    Update After_compile.sh

--- After_compile.sh
+++ After_compile.sh
@@ -1,1 +1,5 @@
+mv ./packages/qbit/*.ipk ./openwrt/bin/packages/
+cd openwrt
+ls -la ./bin/targets/ramips/mt7621
 make image PACKAGES="qbittorrent luci-app-qbittorrent luci-i18n-qbittorrent-zh-cn"
+ls -la ./bin/targets//ramips/mt7621
